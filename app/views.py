777d79efbabad680ab180897e4cfc6976978d41f
petermwash <petermwash48@gmail.com>
2018-04-29 16:03:27 +0300

    [FIX] Fix endpoints to make the tests pass

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, g
+from flask import Flask, request, jsonify, g, abort
 from flask_restful import Resource, Api
 from flask_httpauth import HTTPBasicAuth
 import json
@@ -22,36 +22,42 @@ Meal("bb", "snacks", 400.00)
 menu = [
 Meal("chai", "breko", 30.00)
 ]
-orders = []
-
-u_name_table = {u.u_name: u for u in users}
-u_id_table = {u.u_id: u for u in users}
+orders = [
+Order("pet", "burger", 5)
+]
 
 @auth.verify_password
 def verify_password(u_name, password):
-    user = u_name_table.get(u_name)
-    if not user or not user.verify_password(password):
-        return False
-    g.user = user
-    return True
+	user = User.verify_auth_token(u_name)
+	if not user:
+	    for user in users:
+	    	if user.u_name == u_name:
+	    		uzer = user
+	    		if not uzer or not uzer.verify_password(password):
+			        return False
+	g.user = user
+	return True
 
 class UserSignup(Resource):
 
 	def post(self):
-		info = json.loads(request.data)
-		uzer = User(f_name=info.get('f_name'), 
-			l_name=info.get('l_name'),
-			email=info.get('email'), 
-			u_name=info.get('u_name'), password=info.get('password'))
-		if info.get('u_name') is None or info.get('password') is None \
-				or info.get('f_name') is None \
-				or info.get('l_name') is None or info.get('email') is None: 
-			response = jsonify({"message": "missing argument"})
+		f_name = request.json.get('f_name')
+		l_name = request.json.get('l_name')
+		email = request.json.get('email')
+		u_name = request.json.get('u_name')
+		password = request.json.get('password')
+		if u_name is None or password is None or f_name is None\
+				or l_name is None or email is None: 
+			response = jsonify({"message": "Missing argument"})
 			response.status_code = 400
 			return response
-			uzer.hash_password(info.get('password'))
+		for u in users:
+			if u.u_name == u_name:
+				response = jsonify({"message": "User already exists"})
+				response.status_code = 400
+				return response
+		uzer = User(f_name, l_name, email, u_name, password)
 		users.append(uzer)
-
 		response = jsonify({"message": "User created"})
 		response.status_code = 201
 		return response
@@ -59,24 +65,23 @@ class UserSignup(Resource):
 class UserSignin(Resource):
 
 	@auth.login_required
-	def post(self, u_id):
-		info = json.loads(request.data)
-		for user in users:
-			if user.u_id == u_id:
-				if user.u_name == info.get('u_name')\
-				 and user.password == info.get('password'):
-					response = jsonify({"message": "User loged in"})
-					response.status_code = 200
-					return response
-				else:
-					response = jsonify({
-							"message": "wrong username or password"
+	def post(self):
+		u_name = request.json.get('u_name')
+		password = request.json.get('password')
+		for usr in users:
+			if usr.u_name == u_name:
+				verify_password(u_name, password)
+				token = g.user.generate_auth_token(600)
+				response = jsonify({"token": token.decode('ascii'),
+									"duration": 600,
+									"message": "User loged in"})
+				return response
+			response = jsonify({
+							"message": "Wrong password or username"
 							})
-					response.status_code = 401
-					return response
-			response = jsonify({"message": "User not found"})
-			response.status_code = 404
+			response.status_code = 401
 			return response
+		
 
 class Meals(Resource):
 
@@ -92,12 +97,17 @@ class Meals(Resource):
 		return response
 
 	def post(self):
-		info = json.loads(request.data)
-		meal = Meal(meal_name=info.get('meal_name'), 
-			meal_category=info.get('meal_category'),
-			meal_price=info.get('meal_price'))
+		meal_name = request.json.get('meal_name')
+		meal_category = request.json.get('meal_category')
+		meal_price = request.json.get('meal_price')
+		if meal_name is None or meal_category is None\
+				or meal_price is None: 
+			response = jsonify({"message": "Missing argument"})
+			response.status_code = 400
+			return response
+		meal = Meal(meal_name, meal_category, meal_price)
 		meals.append(meal)
-		response = jsonify({"message": "Meal created"})
+		response = jsonify({"message": "Meal added"})
 		response.status_code = 201
 		return response
 
@@ -106,34 +116,39 @@ class Meals(Resource):
 			if meal.meal_id == meal_id:
 				meals.remove(meal)
 				info = json.loads(request.data)
-				meal = Meal(meal_id,
+				meal = Meal(
 					meal_name=info.get('meal_name'), 
 					meal_category=info.get('meal_category'),
 					meal_price=info.get('meal_price'))
-		meals.append(meal)
-		response = jsonify({"message": "Meal updated"})
-		response.status_code = 200
-		return response
+				meals.append(meal)
+				response = jsonify({"message": "Meal updated"})
+				response.status_code = 200
+				return response
 
 	def delete(self, meal_id):
 		for meal in meals:
 			if meal.meal_id == meal_id:
 				meals.remove(meal)
-				response = jsonify({"message": "meal deleted"})
+				response = jsonify({"message": "Meal deleted!"})
 				response.status_code = 200
 				return response
-			response = jsonify({"message": "meal do not exist"})
+			response = jsonify({"message": "Meal not found!"})
 			response.status_code = 404
 			return response
 
 class MenuEndpoints(Resource):
 
 	def post(self):
-		info = json.loads(request.data)
-		meal = Menu(meal_id=info.get('meal_id'),
-			meal_name=info.get('meal_name'), 
-			meal_category=info.get('meal_category'),
-			meal_price=info.get('meal_price'))
+		meal_id = request.json.get('meal_id')
+		meal_name = request.json.get('meal_name')
+		meal_category = request.json.get('meal_category')
+		meal_price = request.json.get('meal_price')
+		if meal_id is None or meal_name is None or meal_category is None\
+				or meal_price is None: 
+			response = jsonify({"message": "Missing argument"})
+			response.status_code = 400
+			return response
+		meal = Menu(meal_id, meal_name, meal_category, meal_price)
 		menu.append(meal)
 		response = jsonify({"message": "Meal added to menu"})
 		response.status_code = 201
@@ -153,10 +168,15 @@ class MenuEndpoints(Resource):
 class OrderEndpoints(Resource):
 
 	def post(self):
-		info = json.loads(request.data)
-		order = Order(order_owner_name=info.get('order_owner_name'), 
-			meal_name=info.get('meal_name'),
-			quantity=info.get('quantity'))
+		order_owner_name = request.json.get('order_owner_name')
+		meal_name = request.json.get('meal_name')
+		quantity = request.json.get('quantity')
+		if order_owner_name is None or meal_name is None\
+				or quantity is None: 
+			response = jsonify({"message": "Missing argument"})
+			response.status_code = 400
+			return response
+		order = Order(order_owner_name, meal_name, quantity)
 		orders.append(order)
 		response = jsonify({"message": "Order created"})
 		response.status_code = 201
@@ -188,7 +208,7 @@ class OrderEndpoints(Resource):
 
 
 api.add_resource(UserSignup, '/api/v1/signup')
-api.add_resource(UserSignin, '/api/v1/signin/<int:u_id>')
+api.add_resource(UserSignin, '/api/v1/signin')
 api.add_resource(Meals, '/api/v1/meals', '/api/v1/meals/<int:meal_id>')
 api.add_resource(MenuEndpoints, '/api/v1/menu')
-api.add_resource(OrderEndpoints, '/api/v1/orders')
+api.add_resource(OrderEndpoints, '/api/v1/orders', '/api/v1/orders/<int:order_id>')
