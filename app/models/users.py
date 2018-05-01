d3a521794faa5cd1f958035c158d8587c89d6192
petermwash <petermwash48@gmail.com>
2018-05-01 15:27:38 +0300

    [FIX] Fix import error  on users.py

--- app/models/users.py
+++ app/models/users.py
@@ -5,10 +5,8 @@ import random
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
 
-from app import create_app
 
-
-app = create_app()
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisismysupersecrretkeyformyapiauthentications'
 
 class User(object):
